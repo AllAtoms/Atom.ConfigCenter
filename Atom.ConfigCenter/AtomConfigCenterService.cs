f00580cf1f57de5a9cad7e1432d012e83e0abecb
Jerry <728246447@qq.com>
2018-05-22 18:59:31 +0800

    edit note

--- Atom.ConfigCenter/AtomConfigCenterService.cs
+++ Atom.ConfigCenter/AtomConfigCenterService.cs
@@ -27,8 +27,24 @@ namespace Atom.ConfigCenter
             }
         }
 
+        /// <summary>
+        /// 获取全局配置
+        /// </summary>
         public static AtomConfigCenterService Conf = new AtomConfigCenterService();
 
+        /// <summary>
+        /// 添加全局配置
+        /// </summary>
+        /// <param name="code">配置编码</param>
+        /// <param name="value">配置值</param>
+        /// <param name="pCode">上级编码</param>
+        /// <param name="desc">配置描述</param>
+        /// <param name="cType">配置类型、分类、环境</param>
+        /// <param name="st">生效开始时间</param>
+        /// <param name="et">生效结束时间</param>
+        /// <param name="extVal">扩展值</param>
+        /// <param name="isAdd">是否是添加操作</param>
+        /// <returns></returns>
         public static long Set(string code, string value, string pCode = null, string desc = null, string cType = null, DateTime? st = null, DateTime? et = null, string extVal = null, bool isAdd = false)
         {
             if (string.IsNullOrWhiteSpace(code))
@@ -52,6 +68,15 @@ namespace Atom.ConfigCenter
             return AtomConfigCenterManage.Set(ac, isAdd);
         }
 
+        /// <summary>
+        /// 添加配置字段
+        /// </summary>
+        /// <param name="name">字段名</param>
+        /// <param name="code">字段编码</param>
+        /// <param name="pCode">上级编码</param>
+        /// <param name="extCode">扩展编码</param>
+        /// <param name="isAdd">是否添加操作</param>
+        /// <returns></returns>
         public static long SetCate(string name, string code, string pCode = null, string extCode = null, bool isAdd = false)
         {
             if (string.IsNullOrWhiteSpace(name))
@@ -71,6 +96,17 @@ namespace Atom.ConfigCenter
             return AtomConfigCenterManage.SetCate(acc, isAdd);
         }
 
+        /// <summary>
+        /// 添加配置值
+        /// </summary>
+        /// <param name="cateCode">配置字段编码</param>
+        /// <param name="value">字段配置值</param>
+        /// <param name="relId">关联ID</param>
+        /// <param name="extVal">扩展值</param>
+        /// <param name="st">生效开始时间</param>
+        /// <param name="et">生效结束时间</param>
+        /// <param name="isAdd">是否是添加操作</param>
+        /// <returns></returns>
         public static long SetVal(string cateCode, string value, int relId = 0, string extVal = null, DateTime? st = null, DateTime? et = null, bool isAdd = false)
         {
             if (string.IsNullOrWhiteSpace(cateCode))
@@ -93,6 +129,11 @@ namespace Atom.ConfigCenter
             return AtomConfigCenterManage.SetVal(acv, isAdd);
         }
 
+        /// <summary>
+        /// 获取全局配置
+        /// </summary>
+        /// <param name="code">全局配置编码</param>
+        /// <returns></returns>
         public static AtomConfigModel Get(string code)
         {
             var res = AtomConfigCenterManage.Get(code);
@@ -100,6 +141,11 @@ namespace Atom.ConfigCenter
             return res;
         }
 
+        /// <summary>
+        /// 获取当前配置以及下一层子配置
+        /// </summary>
+        /// <param name="code">配置编码</param>
+        /// <returns></returns>
         public static AtomConfigModel Gets(string code)
         {
             var res = AtomConfigCenterManage.Gets(code);
@@ -107,18 +153,34 @@ namespace Atom.ConfigCenter
             return res;
         }
 
+        /// <summary>
+        /// 根据配置字段编码获取子配置字段
+        /// </summary>
+        /// <param name="pCode">上级配置编码</param>
+        /// <returns></returns>
         public static AtomCateConfigModel GetCate(string pCode)
         {
             var result = AtomConfigCenterManage.GetCate(pCode);
             return result;
         }
 
+        /// <summary>
+        /// 获取字段值：没有则使用默认值，默认值relId为0
+        /// </summary>
+        /// <param name="code">字段编码</param>
+        /// <param name="relId">关系ID</param>
+        /// <returns></returns>
         public static AtomConfigValueModel GetVal(string code, int relId)
         {
             var result = AtomConfigCenterManage.GetVal(code, relId);
             return result;
         }
 
+        /// <summary>
+        /// 获取子配置字段值组：没有则使用默认值，默认值relId为0
+        /// </summary>
+        /// <param name="pCode">上级字段编码</param>
+        /// <param name="relId">关系ID</param>
         public static List<AtomConfigValueModel> GetVals(string pCode, int relId)
         {
             var result = AtomConfigCenterManage.GetVals(pCode, relId);
